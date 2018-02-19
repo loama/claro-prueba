8f79bdefb149943e2ad3bfcfaa2f7eb1ea688091
Jaime López <loama18@gmail.com>
2018-02-18 19:25:35 -0600

    add modal with hardcoded info
    added modal and modal overlay, with opening and closing functions and css styles.
    Also added onclick event to every <li>

--- src/components/app.js
+++ src/components/app.js
@@ -12,7 +12,7 @@ class App extends Component {
   renderMoviesList() {
     return this.props.movies.map((movie) => {
       return (
-        <li key={movie.title}>
+        <li key={movie.title} onClick={() => this.openModal(movie.id)}>
           <img src={movie.image_small}></img>
           <div className="hover">
             <div>{movie.title} </div>
@@ -25,6 +25,26 @@ class App extends Component {
   handleChange(event) {
     //console.log(event.target.value);
     this.props.showMovies(event.target.value)
+  }
+
+  openModal(modal) {
+    console.log(modal)
+    //this.props.showModal(modal)
+    document.getElementById('modal').style.transform = 'translate3d(0, 0, 0)'
+    document.getElementById('modalOverlay').style.transform = 'translate3d(0, 0, 0)'
+    setTimeout(function () {
+      document.getElementById('modal').style.opacity = 1
+      document.getElementById('modalOverlay').style.opacity = 1
+    }, 100)
+  }
+
+  hideModal() {
+    document.getElementById('modal').style.opacity = 0
+    document.getElementById('modalOverlay').style.opacity = 0
+    setTimeout(function () {
+      document.getElementById('modal').style.transform = 'translate3d(0, -3000px, 0)'
+      document.getElementById('modalOverlay').style.transform = 'translate3d(0, -3000px, 0)'
+    }, 400)
   }
 
   render() {
@@ -44,6 +64,18 @@ class App extends Component {
             <span className="line line-9"></span>
           </div>
         </div>
+        <div className="modal" id="modal">
+          <img src="https://clarovideocdn9.clarovideo.net/PELICULAS/BUFFYTHEVAMPIRESLAYER/EXPORTACION_WEB/SS/BUFFYTHEVAMPIRESLAYERWVERTICAL.jpg?size=200x300"></img>
+          <div className="title"> La caja </div>
+          <div className="description"> Los Lewis son una familia normal pero lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis nibh mollis, tincidunt sapien et, congue enim. Quisque sed augue erat. Aliquam diam velit, porta sed feugiat vel, scelerisque</div>
+          <div> <span className="tag">Actor:</span> </div>
+          <div> <span className="tag">Director:</span> </div>
+          <div> <span className="tag">Escritor:</span> </div>
+          <div> <span className="tag">Productor:</span> </div>
+          <div> <span className="tag">Género:</span> </div>
+          <div> <span className="tag">Título original:</span> </div>
+        </div>
+        <div className="modalOverlay" id="modalOverlay" onClick={this.hideModal}></div>
         <div className="navbar">
           <img className="logo" src="src/clarovideo-logo.svg"></img>
           <input className="search" type="text" placeholder="buscar..." onChange={this.handleChange.bind(this)}></input>
